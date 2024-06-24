e729ba0e527c92148b03c5a5d870c4482b0d5cdb
Sp0litak <145229434+Sp0litak@users.noreply.github.com>
2024-06-24 13:04:49 +0300

    Add files via upload

---
+++ lab1/student_main.py
@@ -0,0 +1,21 @@
+def task1(str):
+    return len(str)
+
+
+def task2(a , operator, b):
+    if operator == "+":
+        return a + b
+    elif operator == "-":
+        return a - b
+    elif operator == "/":
+        return a / b
+    elif operator == "//":
+        return a // b
+    elif operator == "**":
+        return a ** b
+    elif operator == "*":
+        return a * b
+
+def task3(list):
+    return max(list)
+
